3a06c523795b82d331d533fd13e2918af134aa24
Hugo <h.duporge@motoblouz.com>
2021-11-26 16:25:35 +0100

    add git clone on unknown projet

--- run-dependencies.php
+++ run-dependencies.php
@@ -8,30 +8,33 @@ $action = $argv[2];
  * @param $action
  * @param string|null $repository
  */
-function execute($current, $action, $repository = null)
+function execute($current, $action, string $repository = null)
 {
-    $make = 'cd ../' . $current . ' && make ' . $action . ' ';
-    $git = '';
-    $dependencies = json_decode(file_get_contents('../' . $current . '/' . $current . '_dependencies.json'));
-
-    foreach ($dependencies as $dependency) {
-        if (isset($dependency->path)) {
-            $git = 'git clone '.$dependency->repository;
-            execute($dependency->path, $action, $dependency->repository);
-            if ("front-client" === $current && "start" === $action) {
-                $make .= '&& make serve && cd ../';
-            }
+    if (null === $repository) {
+        $globals = json_decode(file_get_contents('dependencies/global_dependencies.json'));
+        if (!is_dir('../' . $current)) {
+            downloadToRepo($globals->$current->repository);
         }
     }
 
-    if (is_dir('../'.$current)) {
-        print_r('.............................'.$action.'ing '. $current.'.................................'.PHP_EOL);
-        exec($make);
-    } elseif ($repository) {
-        exec($git);
+    $make = 'cd ../' . $current . ' && make ' . $action . ' ';
+    $git = 'git clone ';
+    $projetDependency = '../' . $current . '/' . $current . '_dependencies.json';
+    if (file_exists($projetDependency)) {
+        $dependencies = json_decode(file_get_contents($projetDependency));
+        if (is_iterable($dependencies)) {
+            foreach ($dependencies as $dependency) {
+                execute($dependency->path, $action, $dependency->repository);
+            }
+        }
     }
+    exec($make);
+}
+
+
+function downloadToRepo($repository)
+{
+    exec('git clone '. $repository);
 }
 
 execute($current, $action);
-
-
