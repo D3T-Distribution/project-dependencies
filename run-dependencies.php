16e60a7e37b37f784e104705354b12ea0950406e
Hugo Duporge <h.duporge@motoblouz.com>
2022-02-11 16:36:00 +0100

    fix recursive

--- run-dependencies.php
+++ run-dependencies.php
@@ -8,29 +8,27 @@ $command = $argv[2];
  * @param $action
  * @param string|null $repository
  */
-function execute($project, $command, string $repository = null)
+function runDependencies($project, $command, string $repository = null)
 {
-   return rapido($project, $command, null, 0);
+    return execute($project, $command, null, 0);
 }
 
 
-function downloadToRepo($repository)
+//function downloadToRepo($repository)
+//{
+//    exec('cd .. && git clone ' . $repository);
+//}
+
+function execute($project, $command, string $repository = null, $lvl = 0): string
 {
-    exec('cd .. && git clone ' . $repository);
-}
-
-function rapido($project, $command, string $repository = null, $lvl = 0)
-{
-
-    $dependencies = json_decode(file_get_contents($project.'/'.$project.'_dependencies.json'), true);
-
-    $make = $lvl > 0 ? 'make -C ../'.$project.' '.$command : '';
+    $dependencies = json_decode(file_get_contents($project . '/' . $project . '_dependencies.json'), true);
+    $make = $lvl > 0 ? 'make -C ../' . $project . ' ' . $command : '';
     foreach ($dependencies as $dependency) {
-        $tmp2 =  rapido($dependency['path'], $command,null, ++$lvl);
-        $tmp1 = $tmp2. ($make ? ' && ': '') .$make;
-        $make = $tmp1;
+        $tmp = execute($dependency['path'], $command, null, ++$lvl);
+        $make = $tmp . ($make ? ' && ' : '') . $make;
     }
 
     return $make;
 }
-echo execute($project, $command);
+
+echo runDependencies($project, $command);
