b514e935a80d4a0056c49b1cb8709dd4806cf76c
Hugo Duporge <h.duporge@motoblouz.com>
2022-03-04 17:19:01 +0100

    fix for circular dependencies

--- run-dependencies.php
+++ run-dependencies.php
@@ -2,24 +2,23 @@
 
 $project = $argv[1];
 $command = $argv[2];
+$latests = $argv[3] ?? null;
 
-function runDependencies(string $project, string $command): string
+function runDependencies($project, $command, $latests = null): string
 {
-    return execute($project, $command);
-}
-
-function execute($project, $command): string
-{
+    $latests = $latests ? $project . ',' . $latests : $project;
     $dependencies = json_decode(file_get_contents($project . '/' . $project . '_dependencies.json'), true);
     foreach ($dependencies as $dependency) {
-        if (is_dir($dependency['path'])) {
-            return 'make -C ../' . $dependency['path'] . ' ' . $command. ' ';
-        } else {
-            return 'cd ../ && git clone ' . $dependency['repository'] . ' && cd ' . $dependency['path'] . ' && make install && make ' . $command;
+        if (!str_contains($latests, $dependency['path'])) {
+            if (is_dir($dependency['path'])) {
+                return 'make -C ../' . $dependency['path'] . ' DEPENDENCIES=' . $latests . ' ' . $command;
+            } else {
+                return 'cd ../ && git clone ' . $dependency['repository'] . ' && cd ' . $dependency['path'] . ' && make install && make ' . $command;
+            }
         }
     }
 
     return '';
 }
 
-echo runDependencies($project, $command);
+echo runDependencies($project, $command, $latests);
