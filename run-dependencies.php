c97cff09aec52d9b3e70a094aae2fc5ed0cf3cfe
Hugo Duporge <h.duporge@motoblouz.com>
2022-03-11 14:57:27 +0100

    dockerfile

--- run-dependencies.php
+++ run-dependencies.php
@@ -6,11 +6,13 @@ $latests = $argv[3] ?? null;
 
 function runDependencies($project, $command, $latests = null): string
 {
+    $projectsPath = $_ENV['PROJECTS_PATH'] ?? '/mnt/projects/';
     $latests = $latests ? $project . ',' . $latests : $project;
-    $dependencies = json_decode(file_get_contents($project . '/' . $project . '_dependencies.json'), true);
+
+    $dependencies = json_decode(file_get_contents($projectsPath . $project . '/dependencies.json'), true);
     foreach ($dependencies as $dependency) {
         if (!str_contains($latests, $dependency['path'])) {
-            if (is_dir($dependency['path'])) {
+            if (is_dir($projectsPath . $dependency['path'])) {
                 return 'make -C ../' . $dependency['path'] . ' DEPENDENCIES=' . $latests . ' ' . $command;
             } else {
                 return 'cd ../ && git clone ' . $dependency['repository'] . ' && cd ' . $dependency['path'] . ' && make install && make ' . $command;
